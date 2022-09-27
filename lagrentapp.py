24f913181ad648f8aefdb4ce5ffe63ad15abaf0b
Kehinde Olalekan <86294818+KENNYDGREAT2@users.noreply.github.com>
2022-09-27 17:54:01 +0100

    second app

---
+++ lagrentapp.py
@@ -0,0 +1,96 @@
+#import libraries
+from sklearn import model_selection
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import pickle
+import joblib
+
+
+#load in pipeline
+model_file = 'final_model.sav'
+model = pickle.load(open(model_file,'rb'))
+
+st.set_page_config(
+    layout='wide',
+    page_title='Lagos House Rent Prediction',
+    page_icon='img.jfif',
+)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: shown;}
+            footer {visibility: hidden;}
+            footer:after {
+                          content:'Created by TrailBlazersNG Team'; 
+                          visibility: visible;
+                          display: block;
+                          position: relative;
+                          #background-color: white;
+                          padding: 4px;
+                          top: 2px;
+                          }
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+def set_background(png_file):
+    bin_str = (png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+st.markdown("<h1 style='text-align: center; color: black;'> Lagos House Rent Prediction</h1>", unsafe_allow_html=True)
+image = Image.open('image3.jpg')
+st.image(image, caption='PHOTO FROM ROYALTY-FREE STOCK PHOTO')
+
+def main():
+    
+    # get data and convert data into dataframe
+    with st.form(key='my_form'):
+        Location = st.selectbox('LOCATION:', ['Ikoyi', 'Yaba', 'Lekki', 'Ajah', 'Victoria Island', 'Ikeja',
+                           'Ilupeju', 'Isolo', 'Shomolu', 'Ketu', 'Surulere', 'Amuwo odofin','Abule Egba', 'Oshodi', 
+                            'Apapa','Ikorodu', 'Ojodu', 'Alimosho','Egbeda', 'Ikotun', 'Idimu', 'Ogba', 'Igando', 
+                            'Akowonjo','Ibeju/Lekki', 'Akoka', 'Ebute-metta', 'Ago Palace', 'Gbagada','Oworoshonki', 
+                            'Maryland', 'Ojo', 'Ayobo', 'Agege', 'Marina','Obalende', 'Lagos Island', 'Mushin', 'Magodo', 
+                            'Ojota', 'Ogudu','Epe', 'Bariga', 'Ejigbo', 'Badagry', 'Iganmu'])
+        House_type = st.selectbox('HOUSE_TYPES',['FLAT','DUPLEX', 'BUNGALOW', 'HOUSE'])
+        Number_of_bedrooms = st.number_input('BEDROOMS', min_value=1, max_value=9, value=1)
+        Number_of_bathrooms = st.number_input('BATHROOMS', min_value=1, max_value=9, value=1)
+        Number_of_toilets = st.number_input('TOILETS', min_value=1, max_value=9, value=1)
+        House_Condition = st.selectbox('HOUSE_CONDITION',['NEW', 'OLD'])
+        Furnishing = st.selectbox('FURNISHED',['YES', 'NO'])
+        Servicing = st.selectbox('SERVICED',['YES', 'NO'])
+        
+        submit_button = st.form_submit_button(label='Predict Price') 
+        
+         #define dataframe
+    
+    data_df = pd.DataFrame([[Location, House_type,Number_of_bedrooms,Number_of_bathrooms,Number_of_toilets,House_Condition,Furnishing,Servicing]],
+                           columns = ['LOCATION','HOUSE_TYPES', 'BEDROOMS', 'BATHROOMS', 'TOILETS', 'HOUSE_CONDITION', 'FURNISHED','SERVICED'])
+
+    #prediction
+    price = model.predict(data_df)
+    
+    if submit_button:
+        st.write('The predicted price of the property is: ₦{:,.2f}'.format(price[0]))
+main()   
+st.title('Contributors')
+st.text("""
+        This app was developed by:
+        """)
+st.write('- Michael Onabanjo (https://github.com/Onabanjomicheal)')
+st.write('- Babajide Alao (https://github.com/BabajideAlao-knn)')
+st.write('- Paul Adegbite (https://github.com/octopuspaul110)')
+st.write('- Alinta Innocent (https://github.com/aliNtainnocent)')
+st.write('- Kehinde Olalekan (https://github.com/KENNYDGREAT2)')
+
+st.info('During #DSRoom Project challenge under the mentorship of Samson Afolabi (https://twitter.com/samsonafo)')       
